dff5923e3c1ec169a1d591f135afb9edda3e65ad
hashtag-sau <saurabhdubey0211@gmail.com>
2024-11-15 12:07:03 +0530

    added new project firstSpringApp for learning and experimenting Spring

---
+++ firstSpringApp/src/main/java/com/saurabh/firstspringapp/FirstSpringAppApplication.java
@@ -0,0 +1,22 @@
+package com.saurabh.firstspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@RestController
+public class FirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAppApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value="name", defaultValue="World") String name) {
+		return String.format("<h1>Hello %s!<h1>", name);
+	}
+}
